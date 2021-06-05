75bd6f815cea17c30d46d977eeb5fd52e4016463
Ricardo Guevara <rickar2@hotmail.com>
2021-06-05 07:53:56 -0600

    Agregado el JSON origen de datos, agregados dos endpoints al backend, todavia falta que escuche peticiones y un endpoint por defecto

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
+const fs = require('fs');
+
 const express = require('express');
-const app = new express();
+const app = express();
 
 const PORT = 2012
 
+app.get("/obtenerPuntuaciones", (req, res, next)=>{
+    fs.readFile('./datos.json', 'utf8', (err, data)=>{
+        if(err){
+            res.json({
+                error: true,
+                mensajeError: "No se encontró el origen de datos"
+            });
+            return res.end();
+        } 
+        let datos = JSON.parse(data);
+        let respuesta = {
+            error: false,
+            datos
+        };
+        res.status(200).json(respuesta);
+        res.end();
+    });
+});
+
+app.post("/ingresarPuntuacionNueva", (req, res, next)=>{
+    if(!req.body.Serie){
+        res.json({
+            error: true,
+            mensajeError: "El objeto no tiene el formato correcto."
+        });
+        return res.end();
+    }
+    fs.readFile('./datos.json', 'utf8', (err, data)=>{
+        if(err){
+            res.json({
+                error: true,
+                mensajeError: "No se encontró el origen de datos"
+            });
+            return res.end();
+        } 
+        let datos = JSON.parse(data);
+        datos.push(req.body);
+        let respuesta = {
+            error: false,
+            datos
+        };
+        res.status(200).json(respuesta);
+        res.end();
+    });
+});
