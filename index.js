7075b2563d8228407d64100cc6b3671d14973d32
Ricardo Guevara <rickar2@hotmail.com>
2021-06-05 07:56:19 -0600

    Agregado el error 404 y la escucha a peticiones

--- index.js
+++ index.js
@@ -50,3 +50,13 @@ app.post("/ingresarPuntuacionNueva", (req, res, next)=>{
         res.end();
     });
 });
+
+app.use((req, res, next) =>{
+    res.status(404).json({
+        error: true,
+        mensajeError: "La ruta solicitada no es correcta"
+    });
+    res.end();
+});
+
+app.listen(PORT, () => console.log(`Escuchando en localhost:${PORT}`));
